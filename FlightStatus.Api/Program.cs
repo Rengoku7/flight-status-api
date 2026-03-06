020454adc7bb358fcfbbfcb66597b796b3b953ad
Yernur Permebek <epermebek@devexpert.kz>
2026-03-06 19:52:34 +0500

    Initial Commit with basic project files

---
+++ FlightStatus.Api/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => Results.Ok(new { name = "Flight Status API", version = "1.0" }));
+
+app.Run();
